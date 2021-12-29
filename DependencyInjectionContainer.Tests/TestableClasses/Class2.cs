448f4c2798627cc85ba28a66bbfbb76d844f4846
Diana Selyuk <selukdiana@gmail.com>
2021-12-29 15:47:51 +0300

    add testable classes

---
+++ DependencyInjectionContainer.Tests/TestableClasses/Class2.cs
@@ -0,0 +1,12 @@
+﻿using DependencyInjectionContainer.Attribute;
+
+namespace DependencyInjectionContainer.Tests.TestableClasses
+{
+    public class Class2
+    {
+       public Class1 class1 { get; }
+        public Class2(Class1 class1) {
+            this.class1 = class1;
+        }
+    }
+}
